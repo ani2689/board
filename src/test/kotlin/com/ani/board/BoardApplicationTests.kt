1855986b3523ba9f8d3bafc24efa1bc56047f2e3
ani2689 <ojno7777@gmail.com>
2023-04-13 20:08:34 +0900

    :tada: 프로젝트 생성

---
+++ src/test/kotlin/com/ani/board/BoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ani.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
