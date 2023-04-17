9cc741c01e499c72777536de15ee4f674b7c5e8b
ani2689 <ojno7777@gmail.com>
2023-04-17 21:19:19 +0900

    :sparkles: 보드, 유저 리포지토리 생성

---
+++ src/main/kotlin/com/ani/board/domain/board/domain/repository/BoardRepository.kt
@@ -0,0 +1,10 @@
+package com.ani.board.domain.board.domain.repository
+
+import com.ani.board.domain.board.domain.entity.Board
+import com.ani.board.domain.user.domain.entity.User
+import org.springframework.data.repository.CrudRepository
+
+interface BoardRepository : CrudRepository<Board, Long> {
+    fun findByTitle(title: String): List<Board>
+    fun findByWriter(user: User): List<Board>
+}
---
+++ src/main/kotlin/com/ani/board/domain/user/domain/repository/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.ani.board.domain.user.domain.repository
+
+interface UserRepository {
+}
