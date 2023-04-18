977a4e1f7d3d9f9b3b27be772c71503bfbf2ab40
ani2689 <ojno7777@gmail.com>
2023-04-18 09:55:30 +0900

    :art: User 엔티티 변수 이름 변경

--- src/main/kotlin/com/ani/board/domain/user/domain/entity/User.kt
+++ src/main/kotlin/com/ani/board/domain/user/domain/entity/User.kt
@@ -1,24 +1,35 @@
 package com.ani.board.domain.user.domain.entity
 
 import com.ani.board.domain.board.domain.entity.Board
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.Id
-import javax.persistence.OneToMany
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.hibernate.annotations.DynamicUpdate
+import javax.persistence.*
 
 @Entity
+@DynamicUpdate
 class User (
 
     @Id
-    var id: Long = 0,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
 
+    @Column(unique = true)
     var name: String,
 
     var password: String,
 
+    @JsonIgnore
     @OneToMany(fetch = FetchType.LAZY)
-    var boardWritten: List<Board>,
+    var script: List<Board>,
 
+    @JsonIgnore
     @OneToMany(fetch = FetchType.LAZY)
-    var bookmark: List<Board>
-)
+    var bookmark: List<Board>?
+){
+    fun updatePassword(password: String){
+        this.password=password
+    }
+    fun updateName(name: String){
+        this.name = name
+    }
+}
