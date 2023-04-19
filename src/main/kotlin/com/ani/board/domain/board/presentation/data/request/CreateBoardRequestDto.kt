4749b23053ff7a1a84e0403512ece88cf6fe6f97
ani2689 <ojno7777@gmail.com>
2023-04-19 11:37:20 +0900

    :sparkles: Board request Dto 추가

---
+++ src/main/kotlin/com/ani/board/domain/board/presentation/data/request/CreateBoardRequestDto.kt
@@ -0,0 +1,7 @@
+package com.ani.board.domain.board.presentation.data.request4
+
+
+data class CreateBoardRequestDto (
+    val title: String,
+    val detail: String
+)
---
+++ src/main/kotlin/com/ani/board/domain/board/presentation/data/request/UpdateBoardRequestDto.kt
@@ -0,0 +1,7 @@
+package com.ani.board.domain.board.presentation.data.request4
+
+
+data class UpdateBoardRequestDto (
+    val title: String,
+    val detail: String
+)
