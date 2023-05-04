ca16ee2a7d309fcd520e3ef3340661ef232d8394
ani2689 <ojno7777@gmail.com>
2023-05-04 12:07:29 +0900

    :recycle: 필요없는 내용 삭제

--- src/main/kotlin/com/ani/board/domain/board/presentation/data/dto/UpdateBoardDto.kt
+++ src/main/kotlin/com/ani/board/domain/board/presentation/data/dto/UpdateBoardDto.kt
@@ -1,17 +1,6 @@
 package com.ani.board.domain.board.presentation.data.dto;
 
 data class UpdateBoardDto (
-    val id: Long,
     val title: String,
-    val detail: String,
-    val author: UserDto,
-    val bookmark: Int
-){
-        data class UserDto(
-                val id: Long,
-                val name: String,
-                val script: List<BoardDto>?,
-                val bookmark: List<BoardDto>?
-
-        )
-}
+    val detail: String
+)
