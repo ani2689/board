a65043321a426905cf2eb4ae0a8e0abb3049af8a
ani2689 <ojno7777@gmail.com>
2023-05-30 00:01:43 +0900

    :sparkles: 컨버터 추상화

---
+++ src/main/kotlin/com/ani/board/domain/auth/utils/AuthConverter.kt
@@ -0,0 +1,11 @@
+package com.ani.board.domain.auth.utils
+
+import com.ani.board.domain.auth.presentation.data.dto.LoginDto
+import com.ani.board.domain.auth.presentation.data.dto.SignupDto
+import com.ani.board.domain.auth.presentation.data.requestDto.LoginRequestDto
+import com.ani.board.domain.auth.presentation.data.requestDto.signupRequestDto
+
+interface AuthConverter {
+    fun toDto(loginRequestDto: LoginRequestDto): LoginDto
+    fun toDto(signupRequestDto: signupRequestDto): SignupDto
+}
